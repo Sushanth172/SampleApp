fe61ca878c9e100db54108ca34c9b28d2d5cb6c4
sushanth172 <sushanth1063@gmail.com>
2023-12-20 11:44:32 +0530

    Added Debian & Source files

---
+++ Source/camApp/paint.h
@@ -0,0 +1,22 @@
+//#ifndef PAINT_H
+//#define PAINT_H
+
+//#include <QWidget>
+//#include <QPaintEvent>
+//#include <QPainter>
+//#include <QRectF>
+//#include <QImage>
+
+
+//class Widget : public QWidget {
+//    Q_OBJECT
+
+//    public:
+//        Widget(QWidget *parent = 0);
+
+//    protected:
+//        void paintEvent(QPaintEvent* e);
+
+//};
+
+//#endif // PAINT_H
